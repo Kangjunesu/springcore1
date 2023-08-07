5708ac9251db77f7767f92b8fd86019b5458899c
Kangjunesu <kulbit92@naver.com>
2023-08-07 19:21:12 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,14 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //join에서 save를 호출하면 다형성에 의해 MemoryMemberRepository에 있는 save가 호출됨
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
