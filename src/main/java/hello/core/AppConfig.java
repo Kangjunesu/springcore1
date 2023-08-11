a06f8f1b4e2a20a1db15bb52944e3598e7096508
Kangjunesu <kulbit92@naver.com>
2023-08-11 22:27:29 +0900

    Section 3. AppConfig 리팩토링 역할과 구현이 잘 드러나게.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {  // 전체 동작 방식을 구성(config), 구현객체 생성, 연결.
 
     public MemberService memberService() {  //생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());  // MemberServiceImpl 구현체를 생성할때 MemoryMemberRepository를 주입
+        return new MemberServiceImpl(memberRepository());
     }
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),  // 설계에 대한 그림이 구성정보에 드러나게
+                discountPolicy());  // 역할과 구현클래스가 파악하기 쉽게.
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
