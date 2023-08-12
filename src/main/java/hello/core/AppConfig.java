c9e56d0606f8798b6ff83d44d7cfcc77fdcaccac
Kangjunesu <kulbit92@naver.com>
2023-08-12 16:50:21 +0900

    Section 3. 스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  // 설정 정보, 구성 정보
 public class AppConfig {  // 전체 동작 방식을 구성(config), 구현객체 생성, 연결.
 
+    @Bean
     public MemberService memberService() {  //생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),  // 설계에 대한 그림이 구성정보에 드러나게
                 discountPolicy());  // 역할과 구현클래스가 파악하기 쉽게.
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,20 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();  // memberService를 직접 메인메서드에서 생성하는게 아닌 appconfig로
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();  // memberService를 직접 메인메서드에서 생성하는게 아닌 appconfig로
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에 있는 설정 정보를 갖고 스프링이 @Beab붙은 것들을 컨테이너에 넣고 관리
+
+        MemberService memberService =
+                applicationContext.getBean("memberService", MemberService.class); // AppConfig에서 찾을 객체, 타입을 MemberService.class
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
